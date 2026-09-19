e28f634cf677f36a63afb09cc993559e63853332
Pranavkant Nigam <pnigam@ntpc.co.in>
2026-09-19 23:06:54 +0530

    User Repository Completed

---
+++ src/com/company/ithelpdesk/repository/InMemoryUserRepository.java
@@ -0,0 +1,53 @@
+package com.company.ithelpdesk.repository;
+
+import com.company.ithelpdesk.model.user.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InMemoryUserRepository implements UserRepository{
+
+    private Map<Long, User> users = new HashMap<Long, User>();
+
+    @Override
+    public User saveUser(User user) {
+        if(user == null)
+            return null;
+
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User findUserById(long userId) {
+        if(userId <= 0)
+            return null;
+        return users.get(userId);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public boolean userIdExists(long userId) {
+        if(userId <= 0)
+            return false;
+        return users.containsKey(userId);
+    }
+
+    @Override
+    public boolean userEmailExists(String email) {
+        if(email == null || email.isBlank())
+            return false;
+
+        for(User user : users.values()){
+            if(user.getEmail().equalsIgnoreCase(email))
+                return true;
+        }
+        return false;
+    }
+}
---
+++ src/com/company/ithelpdesk/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.company.ithelpdesk.repository;
+
+import com.company.ithelpdesk.model.user.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    User saveUser(User user);
+    User findUserById(long userId);
+    List<User> getAllUsers();
+    boolean userIdExists(long userId);
+    boolean userEmailExists(String email);
+}

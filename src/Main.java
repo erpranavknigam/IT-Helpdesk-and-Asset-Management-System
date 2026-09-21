48c77bc06e7a1e546881d7fe721c76b23099c1d3
= <=>
2026-09-21 14:04:28 +0530

    Wrote dummy main function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my test application.");
+        System.out.println("This is my test application for learning Java.");
     }
 }
